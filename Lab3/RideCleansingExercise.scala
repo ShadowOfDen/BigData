253ac5af254aad949adba573e9dd91ebee341c44
ShadowOfDen <31400073+ShadowOfDen@users.noreply.github.com>
2022-12-20 22:13:38 +0400

    Update RideCleansingExercise.scala

--- Lab3/RideCleansingExercise.scala
+++ Lab3/RideCleansingExercise.scala
@@ -20,28 +20,28 @@ import org.apache.flink.streaming.api.scala._
  */
 object RideCleansingExercise extends ExerciseBase {
   def main(args: Array[String]) {
-    // parse parameters
+    // Параметры синтаксического анализа
     val params = ParameterTool.fromArgs(args)
     val input = params.get("input", ExerciseBase.pathToRideData)
 
-    val maxDelay = 60 // events are out of order by max 60 seconds
-    val speed = 600   // events of 10 minutes are served in 1 second
+    val maxDelay = 60 // Максимальная задержка события - 60 секунд
+    val speed = 600   // Каждую секунду происходит event, продолжительность - 10 минут
 
-    // set up the execution environment
+    // Выполняем настройку среды
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(parallelism)
 
-    // get the taxi ride data stream
+    // Получаем поток данных о поездке на такси
     val rides = env.addSource(rideSourceOrTest(new TaxiRideSource(input, maxDelay, speed)))
 
     val filteredRides = rides
       .filter(r => GeoUtils.isInNYC(r.startLon, r.startLat) && GeoUtils.isInNYC(r.endLon, r.endLat))
 
 
-    // print the filtered stream
+    // Выводим результат
     printOrTest(filteredRides)
 
-    // run the cleansing pipeline
+    // Запускаем отчистку
     env.execute("Taxi Ride Cleansing")
   }
 
